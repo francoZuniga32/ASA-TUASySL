9873691440f0ede6ca71ce5d3ec73d799c28543e
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2026-08-22 16:01:09 -0300

    feat: punto1 - b

--- TP1/punto1.sh
+++ TP1/punto1.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+#!/bin/bash
+
+while getopts "p:" opt; do
+	case $opt in
+		p)
+			tabla=true
+			case $OPTARG in
+				G) gpt=true ;;
+				M) mbr=true ;;
+			esac
+		;;
+	esac
+done
+
 declare -i cant_archivos
 declare -i tam_archivos
+
 echo "Ingrese la cantidad de archivos a generar:" 
 read cant_archivos
 
@@ -12,6 +27,18 @@ mkdir archivos
 cd archivos
 
 for i in $( seq 1 $cant_archivos); do
-	echo "generando el archivo $i ..." 
-	truncate -s "${tam_archivos}M" "archivo_$i.iso" 
+	archivo="archivo_$i.iso"
+	echo "generando el archivo $archivo ..." 
+	truncate -s "${tam_archivos}M" "$archivo" 
+	losetup -f "$archivo"
+	if [ $tabla ]; then
+		loop=$( losetup -a | grep "$archivo" | cut -d ':' -f 1 )
+		echo "creando la tabla de particiones: $archivo -> $loop" 
+		if [ $gpt ]; then
+			printf "g\nn\n1\n\n\nw\n" | fdisk "$loop"
+		else
+			printf "o\nn\n\n1\n\n\nw\n" | fdisk "$loop"	
+		fi
+		partprobe "$loop"	
+	fi
 done
