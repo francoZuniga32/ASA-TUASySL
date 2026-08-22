18f11e4f0891f68bd54b465ed07bc27c89910c7e
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2026-08-22 19:26:33 -0300

    feat: punto 1 - c

--- TP1/punto1.sh
+++ TP1/punto1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#!/bin/bash
-
-while getopts "p:" opt; do
+while getopts "p:l" opt; do
 	case $opt in
 		p)
 			tabla=true
@@ -11,34 +9,63 @@ while getopts "p:" opt; do
 				M) mbr=true ;;
 			esac
 		;;
+
+		l)
+			listado=true
+		;;
 	esac
 done
 
-declare -i cant_archivos
-declare -i tam_archivos
+crear(){
 
-echo "Ingrese la cantidad de archivos a generar:" 
-read cant_archivos
+	declare -i cant_archivos
+	declare -i tam_archivos
 
-echo "Ingrese el tamaño de los archivos (MB):" 
-read tam_archivos
+	echo "Ingrese la cantidad de archivos a generar:" 
+	read cant_archivos
 
-mkdir archivos
-cd archivos
+	echo "Ingrese el tamaño de los archivos (MB):" 
+	read tam_archivos
 
-for i in $( seq 1 $cant_archivos); do
-	archivo="archivo_$i.iso"
-	echo "generando el archivo $archivo ..." 
-	truncate -s "${tam_archivos}M" "$archivo" 
-	losetup -f "$archivo"
-	if [ $tabla ]; then
-		loop=$( losetup -a | grep "$archivo" | cut -d ':' -f 1 )
-		echo "creando la tabla de particiones: $archivo -> $loop" 
-		if [ $gpt ]; then
-			printf "g\nn\n1\n\n\nw\n" | fdisk "$loop"
-		else
-			printf "o\nn\n\n1\n\n\nw\n" | fdisk "$loop"	
+	mkdir archivos
+	cd archivos
+
+	for i in $( seq 1 $cant_archivos); do
+		archivo="archivo_$i.iso"
+		echo "generando el archivo $archivo ..." 
+		truncate -s "${tam_archivos}M" "$archivo" 
+		losetup -f "$archivo"
+		if [ $tabla ]; then
+			loop=$( losetup -a | grep "$archivo" | cut -d ':' -f 1 )
+			echo "creando la tabla de particiones: $archivo -> $loop" 
+			if [ $gpt ]; then
+				printf "g\nn\n1\n\n\nw\n" | fdisk "$loop"
+			else
+				printf "o\nn\n\n1\n\n\nw\n" | fdisk "$loop"	
+			fi
+			partprobe "$loop"	
 		fi
-		partprobe "$loop"	
-	fi
-done
+	done
+
+}
+
+eliminar(){
+	# eliminamos el dispositivo seleccionado
+	archivo=$(losetup "$1" | cut -d '(' -f 2 | cut -d ')' -f 1 )
+	echo $1 $archivo
+	losetup -d "$1" 
+	rm "$archivo"
+}
+
+if [ $listado ]; then
+	echo "Seleccione un dispositivo a eliminar (loop y archivo)"
+	select disp in $(losetup -a | cut -d ':' -f 1)
+	do
+		eliminar $disp
+		break
+	done
+	
+else
+	crear
+fi
+
